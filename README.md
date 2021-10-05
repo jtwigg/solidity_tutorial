9ed2a7a3fb8a9f5ad6ba2a4c17d0e8cc86433fc8
crankcaseaudio <john.twigg@gmail.com>
2021-10-04 20:53:06 -0700

    Mocha explorer tests

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+### Mocha Explorer:
+https://hardhat.org/guides/vscode-tests.html
+
+`.vscode/settings.json` 
+```
+{
+    "mochaExplorer.files": "test/**/*.{j,t}s",   
+}
+```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,15 @@
 import '@nomiclabs/hardhat-waffle'
 import '@typechain/hardhat'
 import { HardhatUserConfig } from 'hardhat/types'
+import { task } from 'hardhat/config'
+
+task("hello", "Prints 'Hello, World!'", async function (
+  taskArguments,
+  hre,
+  runSuper
+) {
+  console.log("Hello, World!");
+});
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
